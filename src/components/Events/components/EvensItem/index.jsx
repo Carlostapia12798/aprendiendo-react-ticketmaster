be160ecf4f4f75078bee3ec912ef73357c7d0371
Carlos <carlosjtapia12798@hotmail.com>
2025-05-25 12:23:59 -0400

    se paso un evento como parametro en eventItem
    -Dentro del componente eventItem se paso como parametro o propiedad (en
    react) el evento "onClick" como lo estoy definiendo lo puedo llamar como
    quiera en este caso "oneEventClick" para que se entienda
    
    y se define lo que hara este evento en el componente padre que es quien
    lo maneja

--- src/components/Events/components/EvensItem/index.jsx
+++ src/components/Events/components/EvensItem/index.jsx
@@ -1,6 +1,6 @@
-const EventItiem = ({ info,name,image }) => {
+const EventItiem = ({ info, id, name, image, onEventClick }) => {
     return (
-        <div>
+        <div onClick={() => onEventClick(id)}>
             <img src={image} alt={name} width={200} height={200} />
             <h4>{name}</h4>
             <p>{info}</p>
--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -9,15 +9,23 @@ import EventsJSON from "../../data/events.json";
 const Events = () => {
     const [data] = useState(EventsJSON);
     const events = data._embedded.events;
-    
+
+    const handleEventItemClick = (id) => {
+        console.log('evento clickeado: ', id);
+    };
+
     const eventsComponent = events.map((evenItem) => (
         <EventItiem 
             key={`event-item-${evenItem.id}`}
             name={evenItem.name}
             info={evenItem.info}
             image={evenItem.images[0].url}
+            onEventClick={handleEventItemClick}
+            id={evenItem.id}
         />
     ));
+
+
     return (
         <div>
             Eventos
