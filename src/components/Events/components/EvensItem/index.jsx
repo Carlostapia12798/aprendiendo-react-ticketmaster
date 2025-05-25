af4712e5a23611c8d8dad1f54dd642ea21c47e90
Carlos <carlosjtapia12798@hotmail.com>
2025-05-25 12:57:57 -0400

    Se agrego el evento para detener propagacion

--- src/components/Events/components/EvensItem/index.jsx
+++ src/components/Events/components/EvensItem/index.jsx
@@ -1,10 +1,16 @@
 const EventItiem = ({ info, id, name, image, onEventClick }) => {
+    
+const handlerSeeMoreClick = (evt) => { 
+                evt.stopPropagation();
+                onEventClick(id)
+                };
+
     return (
         <div>
             <img src={image} alt={name} width={200} height={200} />
             <h4>{name}</h4>
             <p>{info}</p>
-            <button onClick={() => onEventClick(id)}>Ver mas</button>
+            <button onClick={handlerSeeMoreClick}>Ver mas</button>
         </div>
     );
 };
