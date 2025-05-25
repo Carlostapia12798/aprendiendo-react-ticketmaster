0c1e009c4faa9df3ac8b92f1c1df8b6ded2bd14a
Carlos <carlosjtapia12798@hotmail.com>
2025-05-25 15:58:39 -0400

    Instalacion de dependencia React Hook Form
    -Se instalo la dependencia React Hook Form, para manejar los formularios de mejor manera

--- src/components/SignupForm/index.jsx
+++ src/components/SignupForm/index.jsx
@@ -1,40 +1,35 @@
-import { useState } from "react";
+
+import { useForm } from "react-hook-form";
 
 const SignupForm = () => {
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
-    const [address, setAddress] = useState('');
-    const [phone, setPhone] = useState('');
+    const {register, handleSubmit, reset, formState: {errors}} = useForm();
+    
 
     const handleClearClick = () => {
-        setName('');
-        setAge('');
-        setAddress('');
-        setPhone('');
+        reset();
     };
 
-    const handleSubmitForm = (evt) => {
-        evt.preventDefault();
+    const handleSubmitForm = (data) => {
+        console.log(data);
     }
 
     return (
-        <form onSubmit={handleSubmitForm}>
+        <form onSubmit={handleSubmit(handleSubmitForm)}>
             <label>
                 Name:
-                <input value ={name} onChange={(evt) => setName(evt.target.value)} required/>
+                <input {...register('name', { required:true })} />
             </label>
              <label>
                 Age:
-                <input value ={age} onChange={(evt) => setAge(evt.target.value)}
-                required/>
+                <input {...register('age', { required:true })} />
             </label>
              <label>
                 Address:
-                <input value ={address} onChange={(evt) => setAddress(evt.target.value)} required/>
+                <input {...register('address',{ required:true })} />
             </label>
             <label>
                 Phone:
-                <input value ={phone} onChange={(evt) => setPhone(evt.target.value)} required/>
+                <input {...register('phone',{ required:true })} />
             </label>
             <div>
                 <button type="button" onClick={handleClearClick}>Clear</button>
